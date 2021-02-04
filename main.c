68bccc38237cacc573d7b233680c71279c3ee39d
JH9508 <una147@nate.com>
2021-02-04 21:40:30 +0900

    fists

---
+++ main.c
@@ -0,0 +1,14 @@
+//
+//  main.c
+//  runC
+//
+//  Created by 김지훈 on 2021/02/04.
+//
+
+#include <stdio.h>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    printf("Hello, World!\n");
+    return 0;
+}
