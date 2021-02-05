6f7fa4246669fef1264bb0b336efadb6206f7915
JH9508 <una147@nate.com>
2021-02-05 12:39:23 +0900

    입력받기

--- main.c
+++ main.c
@@ -13,19 +13,26 @@
 // n은 10까지 가능
 
 int main() {
-   // char o; // 기호 입력
-   // int x, y; // 입력 받을 수
-    int max, i;
-    int arr[10];
-    printf("사칙연산할 횟수를 입력하세요 최대 10개\n");
-    scanf("%d",&max);
-    max = max - 1;
-    for (i=0; i<max; i++) {
-        scanf("%d\n",&arr[i]);
+    char o; // 기호 입력
+    
+    int i ,max;
+    int arr[10]; // 최대 정수 개수
+    
+    printf("사칙연산할 개수를 입력하세요 최대 10개 \n");
+    scanf("%d", &max);
+  
+    
+    for (i=1; i<=max; i++) {
+        printf("%d 번째 수 입력해주세요\n",i);
+        scanf("%d",&arr[i]);
     }
     
+    printf(" +  -  *  / 중 1개의 기호를 입력해주세요\n");
+    scanf(" %c", &o);
+    printf("%c", o);
     
- 
+    
     
     return 0;
 }
+
