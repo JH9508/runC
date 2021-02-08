cf767a0fe6d8885935d1bc8e1487cc7474494097
JH9508 <una147@nate.com>
2021-02-09 00:09:32 +0900

    pointer study

---
+++ pointer.c
@@ -0,0 +1,53 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+// x와 y를 바꾸고 싶은 함수
+void swap(int * x, int * y)
+{
+    int temp;
+    printf("%d %d\n", *x, *y);
+    temp = *x;
+    *x = *y;
+    *y = temp;
+
+}
+
+int main(void)
+{
+    int a = 5, b = 10;  // 4바이트 변수 두개
+    int * arr;
+    int n;
+    
+    scanf("%d", &n);            // 사용자가 입력받을 항 갯수 입력
+    
+    
+//    scanf("%d", &n);
+////    arr = malloc(sizeof(int) * n);
+//
+//    for(int i=0;i<n;i++){
+//        arr[i] = i;
+//        printf("%d ", arr[i]);
+//    }
+    
+//    swap(&a, &b);     // Call by Reference (참조에 의한 호출)
+
+    // 기댓값 10 5
+    printf("Main : %d %d\n", a, b);
+    
+    return 0;
+}
+
+/*
+ &a : a의 주소값에 접근
+ *a :
+ **/
+
+/*
+1. 포인터란?
+2. 포인터 변수의 역할
+3. & 연산자의 역할
+4. Call by Value VS Call by Reference
+5. 포인터 예제 2개정도 찾아서 직접 해설
+6. 스택 영역 vs 힙영역
+7. 힙영역에 읽고 쓰는 것은 왜 더 느린가? (스택에 비해)
+ */
---
+++ pointer.h
@@ -0,0 +1,13 @@
+//
+//  pointer.h
+//  runC
+//
+//  Created by 김지훈 on 2021/02/08.
+//
+
+#ifndef pointer_h
+#define pointer_h
+
+#include <stdio.h>
+
+#endif /* pointer_h */
