81293ef11b802e1ec4ea4d07218f03a27c68e3f7
JH9508 <una147@nate.com>
2021-02-15 21:14:37 +0900

    test coding

---
+++ 7for.c
@@ -0,0 +1,74 @@
+//
+//  7for.c
+//  runC
+//
+//  Created by 김지훈 on 2021/02/15.
+//
+#include <stdio.h>
+//int main(void)
+//{
+//    int num;
+//    printf("정수 입력 : ");
+//    scanf("%d", &num);
+//    
+//    if (num < 0) //num이 0보다 작으면 아래의 코드 실행
+//        printf("입력 값은 0보다 작다. \n");
+//    
+//    if (num > 0) // numdl 0보다 크면 아래의 문장 실행
+//        printf("입력 값은 0보다 크다 \n");
+//    
+//    if(num == 0 ) // num이 0이면 아래의 문장 실행
+//        printf("입력 값은 0이다 \n");
+//    
+//    return 0;
+//        
+//}
+
+
+//int main(vodi) // 입력한 수 만큼 문자 반복 출력
+
+//{
+//    int num;
+//    int i = 0;
+//
+//    printf("문자를 출력할 횟수를 양의 정수 입력하시오\n");
+//    scanf("%d",&num);
+//    while (i<num) {
+//        printf("hello world \n");
+//        i++;
+//    }
+//    return 0;
+//}
+
+
+//int main(void) // 7과 9의 배수만 출력
+//{
+//    int i;
+//    for(i=0;i<100;i++)
+//    {
+//        if (i%7==0 || i%9==0) {
+//            printf("%d\n",i);
+//        }
+//    }
+//}
+
+//
+//int main(void) // 큰수 먼저 출력
+//{
+//    int num1,num2;
+//    printf("첫 번째의 정수를 입력하시오 \n");
+//    scanf("%d", &num1);
+//    printf("두 번째의 정수를 입력하시오 \n");
+//    scanf("%d", &num2);
+//
+//    if (num1<num2) {
+//        printf("%d %d \n", num2, num1);
+//    }
+//    else if(num1>num2)
+//    {
+//        printf("%d %d \n", num1, num2);
+//    }
+//
+//
+//    return 0;
+//}
--- main.c
+++ main.c
@@ -1,115 +1,115 @@
+////
+////  main.c
+////  runC
+////
+////  Created by 김지훈 on 2021/02/04.
+////
 //
-//  main.c
-//  runC
+//#include <stdio.h>
+//#include <stdlib.h>
 //
-//  Created by 김지훈 on 2021/02/04.
 //
-
-#include <stdio.h>
-#include <stdlib.h>
-
-
-// Call By value
-// Call By Reference
-// n은 10까지 가능
-
-int add (int *arr, int max){
-
-
-    int result;
-    int i;
-    result = arr[0];
-    for(i=1; i<max; i++){
-        result += arr[i];
-    }
-
-    return result;
-}
-int sub (int *arr, int max){
-
-    int result;
-    int i;
-    result = arr[0];
-    
-    for(i=1; i<max; i++){
-        result -= arr[i];
-    }
-
-    return result;
-}
-int mul (int *arr, int max){
-
-
-    int result;
-    int i;
-    result = arr[0];
-    
-    for(i=1; i<max; i++){
-        result *= arr[i];
-    }
-
-    return result;
-}
-int divf(int *arr, int max){
-
-    int result;
-    int i;
-    result = arr[0];
-    
-    for(i=1; i<max; i++){
-        result /= arr[i];
-    }
-
-    return result;
-}
-
-
-int main() {
-    char o; // 기호 입력
-    int i;
-    int max;
-    int *arr; // 포인터 선언
-    
-    
-    printf("사칙연산할 개수를 입력하세요 \n");
-    scanf("%d", &max);
-    
-    
-    
-    arr = malloc(sizeof(int) * max); // max * 4byte 만큼을 arr에 매모리크기 할당
-    
-        
-    for (i=0; i<max; i++) {
-        printf("%d 번째 숫자를 입력하세요 \n",i +1);
-        scanf("%d", &arr[i]); // 순차적으로 n의 값 입력
-    }
-    
-    /* 입력 값 확인
-    for (i=0; i<max; i++) {
-        printf("숫자 출력 : %d \n", arr[i]); // 입력된 값 출력
-    }
-     */
-    
-    printf(" +  -  *  / 중 1개의 기호를 입력해주세요\n");
-    scanf(" %c", &o);
-    while (o != '+' && o != '-' && o != '*' && o != '/') {
-        printf("잘못 입력하셨습니다 + - * / 중 다시 입력하세요\n");
-        scanf(" %c", &o);
-    }
-
-    if (o == '+') {
-        printf("%d\n",add(arr, max));
-    }
-    else if (o == '-'){
-        printf("%d\n",sub(arr, max));
-    }
-    else if (o == '*'){
-        printf("%d\n",mul(arr, max));
-    }
-    else{
-        printf("%d\n",divf(arr, max));
-    }
-    
-    return 0;
-}
-
+//// Call By value
+//// Call By Reference
+//// n은 10까지 가능
+//
+//int add (int *arr, int max){
+//
+//
+//    int result;
+//    int i;
+//    result = arr[0];
+//    for(i=1; i<max; i++){
+//        result += arr[i];
+//    }
+//
+//    return result;
+//}
+//int sub (int *arr, int max){
+//
+//    int result;
+//    int i;
+//    result = arr[0];
+//    
+//    for(i=1; i<max; i++){
+//        result -= arr[i];
+//    }
+//
+//    return result;
+//}
+//int mul (int *arr, int max){
+//
+//
+//    int result;
+//    int i;
+//    result = arr[0];
+//    
+//    for(i=1; i<max; i++){
+//        result *= arr[i];
+//    }
+//
+//    return result;
+//}
+//int divf(int *arr, int max){
+//
+//    int result;
+//    int i;
+//    result = arr[0];
+//    
+//    for(i=1; i<max; i++){
+//        result /= arr[i];
+//    }
+//
+//    return result;
+//}
+//
+//
+//int main() {
+//    char o; // 기호 입력
+//    int i;
+//    int max;
+//    int *arr; // 포인터 선언
+//    
+//    
+//    printf("사칙연산할 개수를 입력하세요 \n");
+//    scanf("%d", &max);
+//    
+//    
+//    
+//    arr = malloc(sizeof(int) * max); // max * 4byte 만큼을 arr에 매모리크기 할당
+//    
+//        
+//    for (i=0; i<max; i++) {
+//        printf("%d 번째 숫자를 입력하세요 \n",i +1);
+//        scanf("%d", &arr[i]); // 순차적으로 n의 값 입력
+//    }
+//    
+//    /* 입력 값 확인
+//    for (i=0; i<max; i++) {
+//        printf("숫자 출력 : %d \n", arr[i]); // 입력된 값 출력
+//    }
+//     */
+//    
+//    printf(" +  -  *  / 중 1개의 기호를 입력해주세요\n");
+//    scanf(" %c", &o);
+//    while (o != '+' && o != '-' && o != '*' && o != '/') {
+//        printf("잘못 입력하셨습니다 + - * / 중 다시 입력하세요\n");
+//        scanf(" %c", &o);
+//    }
+//
+//    if (o == '+') {
+//        printf("%d\n",add(arr, max));
+//    }
+//    else if (o == '-'){
+//        printf("%d\n",sub(arr, max));
+//    }
+//    else if (o == '*'){
+//        printf("%d\n",mul(arr, max));
+//    }
+//    else{
+//        printf("%d\n",divf(arr, max));
+//    }
+//    
+//    return 0;
+//}
+//
